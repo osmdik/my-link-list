e7db5277c6f3b0afe1337f1c15fac70cbc967c13
osmdik <daikougun0923@gmail.com>
2023-11-10 08:34:22 +0900

    fix : analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,3 @@
-import { Analytics } from '@vercel/analytics/react';
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -13,7 +12,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Analytics />
       </body>
     </Html>
   );
